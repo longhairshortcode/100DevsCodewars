14a321244a2cc60d8a107f14b4fb40a6ce9d73f3
Rona <longhairshortcode@gmail.com>
2023-09-26 11:41:55 -0700

    starting now all codewars code for all classes in this one file but will date it each day

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//CODEWARS FOR 100DEVS CLASSES!!
+
+
+//Class 36 - 9/26/23
+
+//Task (7kyu)
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+//Answer
+function stray(numbers) {
+    for(let i = 0; i < numbers.length; i++)
+      if(numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){
+        return numbers[i];
+      }
+   }
+
+
+
+
