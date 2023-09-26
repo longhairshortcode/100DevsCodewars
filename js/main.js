4c1718b28d97b5da4d7ff06712bd62cc6e38ca1d
Rona <longhairshortcode@gmail.com>
2023-09-26 12:36:02 -0700

    another cs sept 26 class 36

--- js/main.js
+++ js/main.js
@@ -23,10 +23,22 @@ function stray(numbers) {
 
 function strayDos = nums => nums.reduce((a, b) => a ^ b);
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 //Class 36 - 9/26/23 
 
-//Task
+//Task (8kyu)
+//     Your function takes two arguments:
+
+//     current father's age (years)
+//     current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+
 
 //Answer
 
@@ -34,3 +46,27 @@ function twiceAsOld(dad, son){
     return Math.abs(dad - son * 2);
 }
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//Class 36 - 9/26/23 
+
+//Task (8kyu)
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+
+//Answer
+function testEven(n) {
+    return n % 2 === 0
+   }
