a535bc4f2e00175aedf975864b1cc9c7ab8e9d49
Rona <longhairshortcode@gmail.com>
2023-09-27 21:43:09 -0700

    sept 27 class 36 cw

--- js/main.js
+++ js/main.js
@@ -70,3 +70,22 @@ function twiceAsOld(dad, son){
 function testEven(n) {
     return n % 2 === 0
    }
+
+
+//Class 36 - 9/27/23 
+//Task (8kyu)
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+//Answer:
+
+let isItNeg = num => num < 0 ? num : num * -1
