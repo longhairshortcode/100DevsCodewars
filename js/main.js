29284d342632badf0ed327e3a534bd5f7c1e33d9
Rona <longhairshortcode@gmail.com>
2023-09-28 23:22:45 -0700

    none

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ const reverseSeq = n => {
 //Write a function that will take the number of petals of each 
 //flower and return true if they are in love and false if they aren't.
 
-//answer
+//my answer
 
 function lovefunc(flower1, flower2){
   if (flower1 % 2 === 0 && flower2 % 2 !== 0){
