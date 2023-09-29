3805114030b025546bf3141cc3931db1c3f9b62c
Rona <longhairshortcode@gmail.com>
2023-09-28 23:12:08 -0700

    sept 28 cw

--- js/main.js
+++ js/main.js
@@ -131,3 +131,27 @@ const reverseSeq = n => {
   }
   return myArr;
 }
+
+//class 37
+//sept 28 2023
+
+//task
+//Timmy & Sarah think they are in love, but around where they live, 
+//they will only know once they pick a flower each. If one of the 
+//flowers has an even number of petals and the other has an odd 
+//number of petals it means they are in love.
+
+//Write a function that will take the number of petals of each 
+//flower and return true if they are in love and false if they aren't.
+
+//answer
+
+function lovefunc(flower1, flower2){
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0){
+    return true;
+  }else if (flower2 % 2 === 0 && flower1 % 2 !== 0){
+    return true;
+  }else{
+    return false;
+  }
+  }
