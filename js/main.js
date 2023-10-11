065654bf50f9321595495f692646963e55d37383
Rona <longhairshortcode@gmail.com>
2023-10-10 23:54:40 -0700

    5th for class 40 10-10

--- js/main.js
+++ js/main.js
@@ -415,3 +415,8 @@ function hoopCount (n) {
     return "Keep at it until you get it"
   }    
   }
+
+  // other answer I get
+  function hoopCount (n) {
+    return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+  }
