1ae36880fce60db887350fb1151ca411cb454c7b
Rona <longhairshortcode@gmail.com>
2023-10-13 23:32:19 -0700

    first cw for 10/13 class 44

--- js/main.js
+++ js/main.js
@@ -507,3 +507,36 @@ function sum (numbers) {
  let sum = numbers.reduce((acc, cur) => acc + cur, 0)
  return sum;
 };
+
+//class 44 10/13
+//task
+// The museum of incredible dull things wants to get rid of some exhibitions. 
+// Miriam, the interior architect, comes up with a plan to remove the most boring 
+// exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+// However, just as she finished rating all exhibitions, she's off to an important 
+// fair, so she asks you to write a program that tells her the ratings of the items 
+// after one removed the lowest one. Fair enough.
+//Given an array of integers, remove the smallest value. Do not mutate the original 
+//array/list. If there are multiple elements with the same value, remove the one with 
+//a lower index. If you get an empty array/list, return an empty array/list.
+//Don't change the order of the elements that are left.
+
+
+//Answer - from chatGPT, needed it and studied it, get it but advanced
+function removeSmallest(numbers) {
+  if (numbers.length === 0) {
+    return [];
+  }
+
+  // Find the minimum value and its index
+  let min = numbers[0];
+  let minIndex = 0;
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < min) {
+      min = numbers[i];
+      minIndex = i;
+    }
+  }
+  const result = [...numbers.slice(0, minIndex), ...numbers.slice(minIndex + 1)];
+  return result;
+}
