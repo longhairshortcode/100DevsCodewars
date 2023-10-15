24ee59c5e0c3de92bcc9d86ab461390f0ca3ac0f
Rona <longhairshortcode@gmail.com>
2023-10-14 23:06:32 -0700

    5th for 10/14

--- js/main.js
+++ js/main.js
@@ -609,3 +609,21 @@ function reverseList(list) {
     function problem(x) {
       return typeof x === "string" ? "Error" : x * 50 + 6;
     }
+
+    //class 44 for 10/14
+    //task
+    //Every day you rent the car costs $40. If you rent the car for 
+    //7 or more days, you get $50 off your total. Alternatively, if 
+    //you rent the car for 3 or more days, you get $20 off your total.
+    //Write a code that gives out the total amount for different days(d).
+
+    //answer
+    function rentalCarCost(d) {
+      if (d >= 7){
+        return (d * 40) - 50;
+      }else if (d >= 3){
+        return (d * 40) - 20;
+      }else if (d > 0){
+        return d * 40;
+      }
+    }
