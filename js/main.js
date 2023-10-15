b7e1b8a816148b0d4c53560b83c241698b70359c
Rona <longhairshortcode@gmail.com>
2023-10-14 21:18:00 -0700

    4th for 10/14

--- js/main.js
+++ js/main.js
@@ -599,3 +599,13 @@ function reverseList(list) {
       revList.push(list[i]);    
       }return revList;
     }
+
+    //class 44 cw for 10/14
+    //task
+    //Make a function that returns the value multiplied by 50 and increased 
+    //by 6. If the value entered is a string it should return "Error".
+
+    //answer
+    function problem(x) {
+      return typeof x === "string" ? "Error" : x * 50 + 6;
+    }
