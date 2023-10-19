9d422a314900c4dcbe0c376824cd5439055b9096
Rona <longhairshortcode@gmail.com>
2023-10-18 22:34:44 -0700

    2nd  for class 46 10/18

--- js/main.js
+++ js/main.js
@@ -780,3 +780,15 @@ function peopleWithAgeDrink(old) {
   : old < 21 ? "drink beer"
   : "drink whisky"
 }
+
+//task 2 for class 46 10/18
+//In this game, the hero moves from left to right. 
+//The player rolls the dice and moves the number of 
+//spaces indicated by the dice two times. Create a function 
+//for the terminal game that takes the current position of the 
+//hero and the roll (1-6) and return the new position.
+
+//answer
+function move (position, roll) {
+  return position + (roll * 2);
+}
