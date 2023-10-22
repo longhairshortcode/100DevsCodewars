794801d531f1546805e3310730863b15c9177b74
Rona <longhairshortcode@gmail.com>
2023-10-21 22:23:45 -0700

    4th  class 38  10/21

--- js/main.js
+++ js/main.js
@@ -973,3 +973,13 @@ function saleHotdogs(n) {
   : (n >= 10) ? (n * 90)
 }
   
+//task 3 10/12
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+//Answer
+//function addFive(num) {
+  let total = num + 5
+  return total
+}
