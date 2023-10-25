cfb9b2fb05c9330e8bb08aa668ddb02c63d0633d
Rona <longhairshortcode@gmail.com>
2023-10-25 12:52:12 -0700

    10/25 class 39

--- js/main.js
+++ js/main.js
@@ -1081,9 +1081,29 @@ function noBoringZeros(n) {
   return n;
 }
 
+
  //task 2 10/25 Class 39
+//  Create a method to see whether the string is ALL CAPS.
+
+//  Examples (input -> output)
+//  "c" -> False
+//  "C" -> True
+//  "hello I AM DONALD" -> False
+//  "HELLO I AM DONALD" -> True
+//  "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+//  "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+//  In this Kata, a string is said to be in ALL CAPS 
+//whenever it does not contain any lowercase letter so any string containing no 
+//letters at all is trivially considered to be in ALL CAPS.
 
 //answer
+String.prototype.isUpperCase = function() {
+  if (this.toString() === this.toUpperCase()){
+    return true;
+  }else{
+    return false;
+  }
+ }
 
 
  //task 3 10/25 Class 39
