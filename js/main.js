31b81fc680b2eabf1e4e7f027493f69cc0f732b5
Rona <longhairshortcode@gmail.com>
2023-10-24 22:37:05 -0700

    10/24  copy

--- js/main.js
+++ js/main.js
@@ -1094,11 +1094,3 @@ function digitize(n) {
  return arr.push(reverseArr[i]);
  }
 
- //answer wrong that i got before above, review it!
-function digitize(n) {
-  let nStr = n.toString()
- let arr = []
- let reverseArr = n.reverse()
- for (let i = 0; i < reverseArr.length; i++)
- return arr.push(reverseArr[i]);
- }
