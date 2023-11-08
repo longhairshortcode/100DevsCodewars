bff62b249d2223db2c87f7337090a6ef80f13a97
Rona <longhairshortcode@gmail.com>
2023-11-07 22:57:00 -0800

    11/7

--- js/main.js
+++ js/main.js
@@ -1292,3 +1292,15 @@ function peopleOnBus(busStops) {
 
   return totalPeople;
 }
+
+//answer
+function peopleOnBus(busStops) {
+  let totalPeople = 0;
+
+  for (let i = 0; i < busStops.length; i++) {
+    totalPeople += busStops[i][0];  // Number of people getting on
+    totalPeople -= busStops[i][1];  // Number of people getting off
+  }
+
+  return totalPeople;
+}
