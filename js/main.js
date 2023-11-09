60b6fc0f8dd66389e46637a612b91894f6aed43b
Rona <longhairshortcode@gmail.com>
2023-11-08 22:33:39 -0800

    11/8

--- js/main.js
+++ js/main.js
@@ -1281,5 +1281,15 @@ var number = function(busStops){
   return passengers;
 }
 
+//COPY
+var number = function(busStops){
+  let passengers = 0
+  for (let i = 0; i < busStops.length; i++){
+    passengers += busStops[i][0]
+    passengers -= busStops[i][1]
+  }
+  return passengers;
+}
+
 
 
