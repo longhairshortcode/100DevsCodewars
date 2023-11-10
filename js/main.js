3e2e85aa770d46f301ed8aa41694544961227620
Rona <longhairshortcode@gmail.com>
2023-11-09 23:58:27 -0800

    11/9

--- js/main.js
+++ js/main.js
@@ -1301,5 +1301,9 @@ function digitize(n) {
   //make n to a string, then to an array, then back to numbers, then reverse the array
   return String(n).split('').map(Number).reverse()
 }
-
+//another answer from cw that I learned and get
+function digitize(n) {
+  //make n to a string, then to an array, then back to numbers, then reverse the array
+  return String(n).split('').map(Number).reverse()
+}
 
