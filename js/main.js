dc264d6d66c8ce6d121839fd532dc9ef645ca18c
Rona <longhairshortcode@gmail.com>
2023-11-09 23:59:30 -0800

    11/9

--- js/main.js
+++ js/main.js
@@ -1310,4 +1310,12 @@ function digitize(n) {
 function digitize(n) {
   //make n to a string, then to an array, then back to numbers, then reverse the array
   return String(n).split('').map(Number).reverse()
+}//another answer from cw that I learned and get
+function digitize(n) {
+  //make n to a string, then to an array, then back to numbers, then reverse the array
+  return String(n).split('').map(Number).reverse()
+}//another answer from cw that I learned and get
+function digitize(n) {
+  //make n to a string, then to an array, then back to numbers, then reverse the array
+  return String(n).split('').map(Number).reverse()
 }
