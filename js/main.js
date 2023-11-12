27c5e22a3a3cc5e0b68ec5ea12d89f1d7cabe5dd
Rona <longhairshortcode@gmail.com>
2023-11-11 22:38:25 -0800

    11/10

--- js/main.js
+++ js/main.js
@@ -1311,13 +1311,5 @@ function stringToArray(string){
   return string.split(" ")
 }
 
-//COPY task 11/10
-//Write a function to split a string and convert it into an array of words.
-//answer
-function stringToArray(string){
-  return string.split(" ")
-}
-
-
 
 
